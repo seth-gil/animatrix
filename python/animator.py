f4e01eb475f28a162fd6c3c1fa79e74ad400c902
uood5 <sethdwoolley@gmail.com>
2019-04-17 00:29:15 +0100

    turned down fps for testing purposes

--- python/animator.py
+++ python/animator.py
@@ -12,7 +12,7 @@ images = [img for img in os.listdir(imgFolder) if img.endswith(".jpg")]
 frame = cv2.imread(os.path.join(imgFolder, images[0]))
 height, width, layers = frame.shape
 
-video = cv2.VideoWriter(video, 0, 24, (width,height))
+video = cv2.VideoWriter(video, 0, 1, (width,height))
 
 for image in images:
     video.write(cv2.imread(os.path.join(imgFolder, image)))
