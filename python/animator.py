fea3e6bfd84212b9972b1661a3cdab576c7368c8
uood5 <sethdwoolley@gmail.com>
2019-04-17 00:32:12 +0100

    Added debugging print statements

--- python/animator.py
+++ python/animator.py
@@ -8,6 +8,8 @@ import sys
 imgFolder = sys.argv[1]
 video = sys.argv[2]
 
+print (imgFolder,video)
+
 images = [img for img in os.listdir(imgFolder) if img.endswith(".jpg")]
 frame = cv2.imread(os.path.join(imgFolder, images[0]))
 height, width, layers = frame.shape
@@ -15,7 +17,8 @@ height, width, layers = frame.shape
 video = cv2.VideoWriter(video, 0, 1, (width,height))
 
 for image in images:
-    video.write(cv2.imread(os.path.join(imgFolder, image)))
+	print(image)
+	video.write(cv2.imread(os.path.join(imgFolder, image)))
 
 video.release()
 print (sys.argv[2])
