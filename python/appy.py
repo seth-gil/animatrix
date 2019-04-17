f825fc52f13b6a1b16bed76cbec3a83677d9bc5c
gilbertVirgo <gilbertjvirgo@gmail.com>
2019-04-17 17:00:01 +0100

    Added CORS

--- python/appy.py
+++ python/appy.py
@@ -1,10 +1,12 @@
 # Main Flask container
 
 from flask import Flask, jsonify, request
+from flask_cors import CORS
 import os
 import cv2
 
 app = Flask(__name__)
+CORS(app, resources={r"/api/*": {"origins": "*"}});
 
 # Animates all files in a folder 
 def AnimateFolder(imgFolder,video):
