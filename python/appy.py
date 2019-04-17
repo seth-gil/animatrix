1def87b6c0332ee1d72bd435af975a18df5b0f03
uood5 <sethdwoolley@gmail.com>
2019-04-17 13:16:53 +0100

    Changed appy.py to save all files given in a server-side directory as jpegs

--- python/appy.py
+++ python/appy.py
@@ -1,13 +1,26 @@
 # Main Flask container
 
 from flask import Flask, jsonify, request
+import os
 
 app = Flask(__name__)
 
 @app.route("/api/v1/video/<string:task_id>",methods=["POST"])
 def Animate(task_id):
-	print("\n\n\n"+str(request.files)+"\n\n\n")
-	return(request.json)
+	try:
+		request.files
+		None
+	except:
+		abort(400)
+	if not os.path.exists(task_id):
+		os.makedirs(task_id)
+
+	i = 0
+	for file in request.files.getlist("request"):
+		file.save(os.path.join(task_id,str(i)+".jpg"))
+		i = i+1
+	print("\n\n\n"+str(request.files.getlist("request"))+"\n\n\n")
+	return(str(request.files))
 
 if __name__ == "__main__":
-	app.run(debug=True) 
+	app.run(debug=True,host="192.168.1.113") 
