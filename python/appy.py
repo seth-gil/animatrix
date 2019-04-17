af64e8769aba168b3d81d4ae7f33baaff1efd0b9
uood5 <sethdwoolley@gmail.com>
2019-04-17 12:26:21 +0100

    Made appy.py accept files and print their data as debugging

--- python/appy.py
+++ python/appy.py
@@ -1,7 +1,13 @@
 # Main Flask container
 
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 
-@app.route("/api/v1/video/<str:task_id>",methods=["POST"])
+app = Flask(__name__)
+
+@app.route("/api/v1/video/<string:task_id>",methods=["POST"])
 def Animate(task_id):
-	print(request.json)
+	print("\n\n\n"+str(request.files)+"\n\n\n")
+	return(request.json)
+
+if __name__ == "__main__":
+	app.run(debug=True) 
