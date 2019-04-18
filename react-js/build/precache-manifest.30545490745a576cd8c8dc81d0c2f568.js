1f155047d7023d3b415c63b66ffa0831c5c8f0b6
gilbertVirgo <gilbertjvirgo@gmail.com>
2019-04-18 19:46:02 +0100

    Removed build from gitignore (temp[?])

---
+++ react-js/build/precache-manifest.30545490745a576cd8c8dc81d0c2f568.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "cf4d416a35a2dfab731be10e52871275",
+    "url": "/static/media/default.cf4d416a.mp4"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "6773f295cfc4ccf70b07",
+    "url": "/static/js/main.05d52730.chunk.js"
+  },
+  {
+    "revision": "d8e7be2d069034809643",
+    "url": "/static/js/2.366df5a8.chunk.js"
+  },
+  {
+    "revision": "d8e7be2d069034809643",
+    "url": "/static/css/2.e0e4081f.chunk.css"
+  },
+  {
+    "revision": "4ddfa7fa4d1c890145615292cc9c88f9",
+    "url": "/index.html"
+  }
+];
